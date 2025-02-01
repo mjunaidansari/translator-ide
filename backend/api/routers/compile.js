f964aa0b67c53a356c1839ee5291cd40173b22b7
Junaid Ansari <junaidshahid.ansari@gmail.com>
2025-02-01 15:41:21 +0530

    Use piston api to run code

--- backend/api/routers/compile.js
+++ backend/api/routers/compile.js
@@ -1,7 +1,49 @@
+const axios = require('axios')
 const compileRouter = require('express').Router()
 
 compileRouter.get('/', async (req, res) => {
   res.send('Compile router works')
+})
+
+compileRouter.post('/', async (req, res) => {
+
+  const { code, lang, input } = req.body;
+
+  const langMap = {
+    'c': {language: 'c', version: '10.2.0'},
+    'cpp': {language: 'c++', version: '10.2.0'}
+  }
+
+  const data = {
+    'language': langMap[lang].language,
+    'version': langMap[lang].version,
+    'files': [
+      {
+        'name': 'main',
+        'content': code
+      }
+    ],
+    'stdin': input,
+    'compile_timeout': 10000,
+    'run_timeout': 3000,
+    'compile_memory_limit': -1,
+    'run_memory_limit': -1
+  }
+
+  const headers = {
+    'Content-Type': 'application/json'
+  }
+
+    axios.post('https://emkc.org/api/v2/piston/execute', data, {
+      headers: headers
+    }).then((response) => {
+        res.json(response.data.run);
+        console.log(response.data);
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).send({ error: "Something went wrong" });
+    });
+
 })
 
 module.exports = compileRouter
--- backend/config/app.js
+++ backend/config/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 // routers 
 const compileRouter = require('../api/routers/compile')
@@ -11,6 +12,8 @@ const app = express()
 app.use(cors({
   origin: "*"
 }))
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/api/compile', compileRouter)
 
